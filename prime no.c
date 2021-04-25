47d73ba62c3c2a10e657d50eeb739827351d7d33
Kritika Saxena <82437949+Kritika05802@users.noreply.github.com>
2021-04-26 00:44:11 +0530

    By Kritika Saxena
    A C program to determine whether the given number is prime or not using recursion.

---
+++ prime no.c
@@ -0,0 +1,46 @@
+//prime no. using recursion-calling of the function again again 
+#include<stdio.h>
+int primenumber(int n,int i); //function decleration
+
+int main()
+{ int n,check;
+  printf("Enter any number: ");
+  scanf("%d",&n);
+  check = primenumber(n,n/2);
+  if(check==1)
+  { printf("%d is a prime number\n",n);
+  }
+  else
+  { printf("%d is NOT a prime number\n",n);
+  }
+return 0;
+}
+
+int primenumber(int n, int i) //calling of the function
+{ if(i==1)
+  {  return 1;}
+  else //if (n%i==0)
+  { if(n%i==0)
+    
+   {  return 0;  } 
+  else
+  { return primenumber(n,i-1);
+  }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
